f4328b8f51ae8baca7cf80ea6ab3d39d451cd110
Brad Garropy <bradgarropy@gmail.com>
2020-02-18 23:52:20 -0600

    template pages.

---
+++ src/pages/episodes.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {Meta, Twitter, Facebook} from "../components/SEO"
+
+const EpisodesPage = () => {
+    return (
+        <>
+            <Meta title="episodes" />
+            <Facebook />
+            <Twitter />
+
+            <h1>episodes</h1>
+        </>
+    )
+}
+
+export default EpisodesPage
---
+++ src/pages/playlists.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {Meta, Twitter, Facebook} from "../components/SEO"
+
+const PlaylistsPage = () => {
+    return (
+        <>
+            <Meta title="playlists" />
+            <Facebook />
+            <Twitter />
+
+            <h1>playlists</h1>
+        </>
+    )
+}
+
+export default PlaylistsPage
