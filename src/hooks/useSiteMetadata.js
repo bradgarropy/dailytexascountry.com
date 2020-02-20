40e2fe24becef41fde15bd0593c1f8ea7bfad893
Brad Garropy <bradgarropy@gmail.com>
2020-02-20 00:24:28 -0600

    use site meta data.

---
+++ src/hooks/useSiteMetadata.js
@@ -0,0 +1,27 @@
+import {useStaticQuery, graphql} from "gatsby"
+
+const useSiteMetadata = () => {
+    const query = graphql`
+        {
+            site {
+                siteMetadata {
+                    siteUrl
+                    title
+                    description
+                    keywords
+                    email
+                    twitter
+                    youtube
+                    instagram
+                }
+            }
+        }
+    `
+
+    const data = useStaticQuery(query)
+    const {siteMetadata} = data.site
+
+    return siteMetadata
+}
+
+export default useSiteMetadata
