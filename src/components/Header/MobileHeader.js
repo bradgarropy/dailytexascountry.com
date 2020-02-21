23f8a40fbaf5e9b0ba74a6995f5be5316b4f1db3
Brad Garropy <bradgarropy@gmail.com>
2020-02-20 23:02:49 -0600

    mobile menu.

--- src/components/Header/MobileHeader.js
+++ src/components/Header/MobileHeader.js
@@ -1,14 +1,22 @@
 import React from "react"
+import {useContext} from "react"
 import {Link} from "gatsby"
 import styled from "styled-components"
 import Navigation from "./Navigation"
+import {AppContext} from "../../context/app"
+import Menu from "../../svg/menu.svg"
 
 const StyledMobileHeader = styled.div`
     color: ${({theme}) => theme.colors.white};
     background: ${({theme}) => theme.colors.blue};
     padding: 2rem;
     display: grid;
-    grid-auto-flow: column;
+    grid-template-columns: auto auto;
+    grid-template-rows: auto 1fr;
+    justify-content: space-between;
+    align-content: start;
+    align-items: center;
+    height: ${({open}) => (open ? "100vh" : "auto")};
 
     @media (min-width: ${({theme}) => theme.breakpoints.mobile}) {
         display: none;
@@ -22,11 +30,22 @@ const StyledLink = styled(Link)`
     justify-self: start;
 `
 
+const StyledMenu = styled(Menu)`
+    max-height: 1.5rem;
+    width: auto;
+    cursor: pointer;
+`
+
 const MobileHeader = () => {
+    const {open, setOpen} = useContext(AppContext)
+
+    const onClick = () => setOpen(!open)
+
     return (
-        <StyledMobileHeader>
+        <StyledMobileHeader open={open}>
             <StyledLink to="/">DTXC</StyledLink>
-            <Navigation />
+            <StyledMenu onClick={onClick} />
+            <Navigation open={open} />
         </StyledMobileHeader>
     )
 }
--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,12 +1,21 @@
 import React from "react"
 import {Link} from "gatsby"
+import {useContext} from "react"
+import PropTypes from "prop-types"
 import styled from "styled-components"
+import {AppContext} from "../../context/app"
 
 const StyledNavigation = styled.div`
-    display: grid;
+    display: ${({open}) => (open ? "grid" : "none")};
     grid-auto-flow: column;
     justify-content: space-between;
+    justify-items: start;
     color: ${({theme}) => theme.colors.white};
+
+    @media (max-width: ${({theme}) => theme.breakpoints.mobile}) {
+        grid-auto-flow: row;
+        gap: 2rem;
+    }
 `
 
 const StyledLink = styled(Link)`
@@ -15,15 +24,34 @@ const StyledLink = styled(Link)`
     font-weight: bold;
 `
 
-const Navigation = () => {
+const Navigation = ({open = true}) => {
+    const {setOpen} = useContext(AppContext)
+
+    const onClick = () => setOpen(false)
+
     return (
-        <StyledNavigation>
-            <StyledLink to="/blog">blog</StyledLink>
-            <StyledLink to="/episodes">episodes</StyledLink>
-            <StyledLink to="/store">store</StyledLink>
-            <StyledLink to="/playlists">playlists</StyledLink>
+        <StyledNavigation open={open}>
+            <StyledLink to="/blog" onClick={onClick}>
+                blog
+            </StyledLink>
+
+            <StyledLink to="/episodes" onClick={onClick}>
+                episodes
+            </StyledLink>
+
+            <StyledLink to="/store" onClick={onClick}>
+                store
+            </StyledLink>
+
+            <StyledLink to="/playlists" onClick={onClick}>
+                playlists
+            </StyledLink>
         </StyledNavigation>
     )
+}
+
+Navigation.propTypes = {
+    open: PropTypes.bool,
 }
 
 export default Navigation
