83fa2fd57b4f79d6c9fc34bf834498362a5e8486
Brad Garropy <bradgarropy@gmail.com>
2020-03-15 13:33:58 -0500

    fix episode slice.

--- src/hooks/useEpisodes.js
+++ src/hooks/useEpisodes.js
@@ -24,9 +24,10 @@ const useEpisodes = ({limit = 0} = {}) => {
     const data = useStaticQuery(query)
 
     let episodes = data.allYoutubeVideo.nodes
+    console.log(episodes)
 
     if (limit) {
-        episodes = episodes.slice(0, limit)
+        episodes = episodes.slice(0, limit + 1)
     }
 
     // truncate description
