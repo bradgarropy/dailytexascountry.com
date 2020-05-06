05a80e893fab90845d327bd9304dee75d4ed1a8b
Brad Garropy <bradgarropy@gmail.com>
2020-05-06 11:00:04 -0500

    home page episode section margin. closes #100.

--- src/components/YouTube.js
+++ src/components/YouTube.js
@@ -1,9 +1,31 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
+
+const StyledYouTube = styled.div`
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    margin: 0 auto;
+
+    ::after {
+        padding-top: 56.25%;
+        display: block;
+        content: "";
+    }
+
+    iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`
 
 const YouTube = ({id}) => {
     return (
-        <div className="youtube">
+        <StyledYouTube>
             <iframe
                 title={id}
                 src={`https://www.youtube-nocookie.com/embed/${id}`}
@@ -11,7 +33,7 @@ const YouTube = ({id}) => {
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             />
-        </div>
+        </StyledYouTube>
     )
 }
 
