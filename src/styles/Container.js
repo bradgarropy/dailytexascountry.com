c0d6a616bd629d59878a7a8924b518dac7ff410d
Brad Garropy <bradgarropy@gmail.com>
2020-05-08 10:42:32 -0500

    remove twitter post.

--- src/styles/Container.js
+++ src/styles/Container.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 const Container = styled.div`
     display: grid;
     justify-content: center;
-    padding: 2rem;
+    padding: 2rem 2rem 6rem 2rem;
 `
 
 export default Container
