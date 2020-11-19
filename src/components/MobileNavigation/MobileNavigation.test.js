b08653a9ced196e511615971f9b2144043a56d78
Brad Garropy <bradgarropy@gmail.com>
2020-11-18 19:59:27 -0600

    progress on navigation test.

--- src/components/MobileNavigation/MobileNavigation.test.js
+++ src/components/MobileNavigation/MobileNavigation.test.js
@@ -1,5 +1,6 @@
 import {useApp} from "hooks"
 import {render, screen} from "test-utils/render"
+import userEvent from "@testing-library/user-event"
 import {generateAppCtx} from "test-utils/generators"
 import MobileNavigation from "components/MobileNavigation"
 
@@ -31,4 +32,14 @@ test("shows open mobile navigation", () => {
             `/${link.toLowerCase()}`,
         ),
     )
+})
+
+test("closes mobile navigation", () => {
+    useApp.mockReturnValue(generateAppCtx({open: true}))
+
+    render(<MobileNavigation />)
+
+    userEvent.click(screen.getByText(links[0].toUpperCase()))
+    expect(mockAppCtx.setOpen).toHaveBeenCalledTimes(1)
+    expect(mockAppCtx.setOpen).toHaveBeenCalledWith(false)
 })
---
+++ src/components/Navigation/Navigation.test.js
@@ -0,0 +1,29 @@
+import {useApp} from "hooks"
+import {mockAppCtx} from "test-utils/mocks"
+import Navigation from "components/Navigation"
+import {render, screen} from "test-utils/render"
+import userEvent from "@testing-library/user-event"
+
+jest.mock("hooks")
+useApp.mockReturnValue(mockAppCtx)
+
+const links = ["posts", "episodes", "store", "playlists"]
+
+test("shows navigation", () => {
+    render(<Navigation />)
+
+    links.forEach(link =>
+        expect(screen.getByText(link.toUpperCase())).toHaveAttribute(
+            "href",
+            `/${link.toLowerCase()}`,
+        ),
+    )
+})
+
+test("closes navigation", () => {
+    render(<Navigation />)
+
+    userEvent.click(screen.getByText(links[0].toUpperCase()))
+    expect(mockAppCtx.setOpen).toHaveBeenCalledTimes(1)
+    expect(mockAppCtx.setOpen).toHaveBeenCalledWith(false)
+})
