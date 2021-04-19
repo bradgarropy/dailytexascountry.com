07e34727a1f2c4621e815ff1a92a736c59a2028a
Brad Garropy <bradgarropy@gmail.com>
2021-04-19 08:55:02 -0500

    formatting.

--- src/components/FeaturedPosts/FeaturedPosts.js
+++ src/components/FeaturedPosts/FeaturedPosts.js
@@ -1,5 +1,5 @@
-import {getImage} from "gatsby-plugin-image"
 import Link from "@bradgarropy/gatsby-link"
+import {getImage} from "gatsby-plugin-image"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 import Image from "styles/Image"
